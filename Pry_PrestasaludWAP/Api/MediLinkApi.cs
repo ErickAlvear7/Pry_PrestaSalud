e9448fe1905214fcaf1420f298c83be12a2f6dc2
ErickAlvear7 <xamaringit@gmail.com>
2025-02-03 12:43:00 -0500

    correcv44

---
+++ Pry_PrestasaludWAP/Api/MediLinkApi.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pry_PrestasaludWAP.Api
+{
+    public class MediLinkApi
+    {
+
+        public string PostAccesLogin(string url,string dataLogin)
+        {
+            try
+            {
+                HttpClient _login = new HttpClient();
+                _login.BaseAddress = new Uri(url);
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                HttpContent _content = new StringContent(dataLogin, Encoding.UTF8, "application/json");
+                var resLogin = _login.PostAsync("api/login", _content).Result;
+                if (resLogin.IsSuccessStatusCode)
+                {
+                    var responseContent = resLogin.Content.ReadAsStringAsync().Result;
+                    dynamic accessToken = JObject.Parse(responseContent);
+                    string token = accessToken.datos.accesToken;
+                    return token;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var mensaje = ex.ToString();
+                new Funciones().funCrearLogAuditoria(1, "MediLinkApi.cs/PostAccesLogin", mensaje, 36);
+            }
+            return "";
+        }
+    }
+}
--- Pry_PrestasaludWAP/CitaMedica/FrmAgendarMediLink.aspx.cs
+++ Pry_PrestasaludWAP/CitaMedica/FrmAgendarMediLink.aspx.cs
@@ -2,18 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Pry_PrestasaludWAP.Api;
+using System.Data;
+using Pry_PrestasaludWAP.Modelo;
+using static Pry_PrestasaludWAP.Modelo.MediLinkModel;
 
 namespace Pry_PrestasaludWAP.CitaMedica
 {
     public partial class FrmAgendarMediLink : System.Web.UI.Page
     {
+
+        string accessToken = "";
+        string idtitular = "";
+        string user = "";
+        string pass = "";
+        Object[] objparam = new Object[1];
+        DataSet dt = new DataSet();
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            string titualr = Request["CodigoTitular"];
+            idtitular = Request["CodigoTitular"];
+            //GET PARAMETROS USUARIO Y PASSWORD MEDILINK
+            objparam[0] = 61;
+            dt = new Conexion(2, "").funConsultarSqls("sp_CargaCombos", objparam);
+            if(dt != null && dt.Tables[0].Rows.Count > 0)
+            {
+                user = dt.Tables[0].Rows[0][1].ToString().Trim();
+                pass = dt.Tables[0].Rows[1][1].ToString().Trim();
+            }
+
+            var login = new LoginApi
+           {
+               username = user,
+               password = pass
+            };
+
+            var data = new JavaScriptSerializer().Serialize(login);
+            accessToken = new MediLinkApi().PostAccesLogin("https://testagendamiento.medilink.com.ec/", data);
+
 
         }
+
+        //GET CEDULA TITULAR VERIFICAR SI EXISTE EN BDD MEDIKINK
+
+
+
+
+
     }
 }
