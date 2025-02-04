5874bec4f0d2841e1adc546b11cf17690a0f4f4f
Cristian Alvear <cfam2212@gmail.com>
2025-02-03 20:30:34 -0500

    cambios realizados

--- Pry_PrestasaludWAP/CitaMedica/FrmAgendarMediLink.aspx.cs
+++ Pry_PrestasaludWAP/CitaMedica/FrmAgendarMediLink.aspx.cs
@@ -27,8 +27,12 @@ namespace Pry_PrestasaludWAP.CitaMedica
         {
 
             idtitular = Request["CodigoTitular"];
-            //GET PARAMETROS USUARIO Y PASSWORD MEDILINK
-            objparam[0] = 61;
+            ViewState["CodigoTitular"] = idtitular;
+
+            //*** ESTO DEBERIA IR EN EL BOTON POR QUE COMO SE HACE POSTBACK SE VUELVE A EJECUTAR EL LOAD Y VUELVE A GENERAR EL TOKEN OTRA VEZ
+
+            ////GET PARAMETROS USUARIO Y PASSWORD MEDILINK
+            /*objparam[0] = 61;
             dt = new Conexion(2, "").funConsultarSqls("sp_CargaCombos", objparam);
             if(dt != null && dt.Tables[0].Rows.Count > 0)
             {
@@ -53,6 +57,7 @@ namespace Pry_PrestasaludWAP.CitaMedica
             dt = new Conexion(2, "").funConsultarSqls("sp_ConsultaDatos", objparam);
             documento = dt.Tables[0].Rows[0][0].ToString();
             lblDocumento.Text = documento;
+            */
 
 
         }
@@ -60,7 +65,34 @@ namespace Pry_PrestasaludWAP.CitaMedica
         protected void Button1_Click(object sender, EventArgs e)
         {
 
-            response = new MediLinkApi().GetVerificarPaciente("https://testagendamiento.medilink.com.ec/", accessToken, documento,"C");
+            //GET PARAMETROS USUARIO Y PASSWORD MEDILINK
+            objparam[0] = 61;
+            dt = new Conexion(2, "").funConsultarSqls("sp_CargaCombos", objparam);
+            if (dt != null && dt.Tables[0].Rows.Count > 0)
+            {
+                user = dt.Tables[0].Rows[0][1].ToString().Trim();
+                pass = dt.Tables[0].Rows[1][1].ToString().Trim();
+            }
+
+            var login = new LoginApi
+            {
+                username = user,
+                password = pass
+            };
+
+            var data = new JavaScriptSerializer().Serialize(login);
+            accessToken = new MediLinkApi().PostAccesLogin("https://testagendamiento.medilink.com.ec/", data);
+
+            //GET CEDULA TITULAR
+            Array.Resize(ref objparam, 3);
+            objparam[0] = int.Parse(ViewState["CodigoTitular"].ToString());
+            objparam[1] = "";
+            objparam[2] = 185;
+            dt = new Conexion(2, "").funConsultarSqls("sp_ConsultaDatos", objparam);
+            documento = dt.Tables[0].Rows[0][0].ToString();
+            lblDocumento.Text = documento;
+
+            response = new MediLinkApi().GetVerificarPaciente("https://testagendamiento.medilink.com.ec/", accessToken, documento, "C");
         }
 
         
